b0626886d7ad1229dbdc0bdeefb54f80db74ca0d
NelsonFrancisco <nelson_cunha1991@hotmail.com>
2021-09-30 16:36:58 +0100

    dev: support 'import' statement

--- tests/lib/rules/no-relative-paths.js
+++ tests/lib/rules/no-relative-paths.js
@@ -8,6 +8,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
+//oh the irony
 var rule = require(`../../../lib/rules/no-relative-paths`),
   RuleTester = require(`eslint`).RuleTester
 
@@ -16,19 +17,22 @@ var rule = require(`../../../lib/rules/no-relative-paths`),
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester()
+var ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2020, sourceType: `module`, } })
 const relativePathMessage = `Require with relative paths symbols`
 const localPathMessage = `Require with local path symbol and option 'allowLocalSymbol' to false`
-const type = `CallExpression`
+const callExpressionErrorType = `CallExpression`
+const importDeclarationErrorType = `ImportDeclaration`
+const importExpressionErrorType = `ImportExpression`
 const allowLocalSymbol = [{allowLocalSymbol:true}]
 
 ruleTester.run(`no-relative-paths`, rule, {
-
   valid: [
     {
       code: `var module = require('src/module')`
     },{
       code: `require('src/module')`
+    },{
+      code: `require(module)`
     },{
       code: `call('../', require('module'))`
     },{
@@ -42,60 +46,128 @@ ruleTester.run(`no-relative-paths`, rule, {
     },{
       code: `require('src/module', './module')`,
       options: allowLocalSymbol
+    },{
+      code: `import foo from 'foo'`,
+    },{
+      code: `import 'foo'`,
+    },{
+      code: `import './foo'`,
+      options: allowLocalSymbol
+    },{
+      code: `import * as stuff from 'foo'`,
+    },{
+      code: `import {stuff} from 'foo'`,
+    },{
+      code: `import (foo)`,
+    },{
+      code: `import ('module')`,
+    },{
+      code: `import ('./foo')`,
+      options: allowLocalSymbol
+    },{
+      code: `const {stuff} = import ('foo')`,
     },
   ],
-
   invalid: [
     {
       code: `var module = require('../src/module')`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }]
     },{
       code: `require('../src/module')`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }]
     },{
       code: `require('src/../module')`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }]
     },{
       code: `call('../', require('../module'))`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }]
     },
     {
       code: `call('../', require('../src/module'))`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }],
       options: allowLocalSymbol
     },{
       code: `require('./src/module')`,
       errors: [{
         message: localPathMessage,
-        type
+        type: callExpressionErrorType
       }],
     },{
       code: `require('src/module', '../module')`,
       errors: [{
         message: relativePathMessage,
-        type
+        type: callExpressionErrorType
       }]
     },{
       code: `require('src/module', './module')`,
       errors: [{
         message: localPathMessage,
-        type
+        type: callExpressionErrorType
       }],
-    },
-  ]
+    },{
+      code: `import '../module'`,
+      errors: [{
+        message: relativePathMessage,
+        type: importDeclarationErrorType
+      }],
+    },{
+      code: `import foo from '../module'`,
+      errors: [{
+        message: relativePathMessage,
+        type: importDeclarationErrorType
+      }],
+    },{
+      code: `import * as stuff from '../module'`,
+      errors: [{
+        message: relativePathMessage,
+        type: importDeclarationErrorType
+      }],
+    },{
+      code: `import foo from './module'`,
+      errors: [{
+        message: localPathMessage,
+        type: importDeclarationErrorType
+      }],
+    },{
+      code: `import ('../module')`,
+      errors: [{
+        message: relativePathMessage,
+        type: importExpressionErrorType
+      }],
+    },{
+      code: `import ('./module')`,
+      errors: [{
+        message: localPathMessage,
+        type: importExpressionErrorType
+      }],
+    },{
+      code: `const {stuff} = import ('./module')`,
+      errors: [{
+        message: localPathMessage,
+        type: importExpressionErrorType
+      }],
+    },{
+      code: `const {stuff} = import ('../module')`,
+      errors: [{
+        message: relativePathMessage,
+        type: importExpressionErrorType
+      }],
+      options: allowLocalSymbol
+    }
+  ],
 })
